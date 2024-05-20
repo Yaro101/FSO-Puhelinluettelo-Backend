6c26280d467c86a676c1878632aa06e4b1197242
Yahya Rouissi <yahyarouissi@gmail.com>
2024-05-20 10:29:34 +0300

    backend-3.9

---  puhelinluettelon backend/index.js
+++  puhelinluettelon backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const PORT = 3001
 const date = new Date()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -98,6 +100,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
